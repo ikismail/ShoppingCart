d407078850ae455f1478d5f88a809be9ec7a7383
Mohammed Ismail <ikismail7@gmail.com>
2016-10-16 14:16:35 +0530

    Screenshots

--- src/main/java/com/controller/CartController.java
+++ src/main/java/com/controller/CartController.java
@@ -1,9 +1,6 @@
 package com.controller;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.Valid;
--- src/main/java/com/service/UserServiceImpl.java
+++ src/main/java/com/service/UserServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dao.UserDao;
-import com.model.Product;
 import com.model.User;
 
 @Service
